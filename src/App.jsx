ad4da463763d93c43e500c88d9810a8c956ada4a
unknown <s4shreyadhingra@gmail.com>
2024-03-30 17:58:05 +0530

    added hover functionality on star rating

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import RandomColorGenerator from "./components/random-color-generator";
 import StarRating from "./components/star-rating";
 
 const App = () => {
-  return <div>
-    {/* <Accordian /> */}
-    {/* <RandomColorGenerator /> */}
-    <StarRating />
-  </div>;
+  return (
+    <div>
+      {/* <Accordian /> */}
+      {/* <RandomColorGenerator /> */}
+      <StarRating numberOfStars={10} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -3,22 +3,25 @@ import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import "./styles.css";
 
-const StarRating = () => {
+const StarRating = ({ numberOfStars }) => {
   const [currentRating, setCurrentRating] = useState(-1);
-
-  const stars = [];
-  for (let i = 0; i < 10; i++) stars.push(<FaStar />);
+  const [hoverValue, setHoverValue] = useState(-1);
 
   return (
     <div className="container">
       <div className="star-rating">
-        {stars.map((star, index) => (
+        {[...Array(numberOfStars)].map((_, index) => (
           <div
-            className={`star ${index <= currentRating && "selected"}`}
+            className={`star ${
+              (index <= hoverValue && "hover-over") ||
+              (hoverValue === -1 && index <= currentRating && "selected")
+            }`}
             key={index}
+            onMouseEnter={() => setHoverValue(index)}
+            onMouseLeave={() => setHoverValue(-1)}
             onClick={() => setCurrentRating(index)}
           >
-            {star}
+            <FaStar />
             <p>{index + 1}</p>
           </div>
         ))}
